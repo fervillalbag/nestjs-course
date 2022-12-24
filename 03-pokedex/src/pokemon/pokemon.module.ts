4e3321bf7598897ed932e82341cb1a3b2c6c93ad
Fernando Villalba <21fervillalbag@gmail.com>
2022-12-24 08:11:42 -0300

    feat: create, update, delete and findOne pokemons with a lot validation

--- 03-pokedex/src/pokemon/pokemon.module.ts
+++ 03-pokedex/src/pokemon/pokemon.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose/dist';
+
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Pokemon.name, schema: PokemonSchema }]),
+  ],
   controllers: [PokemonController],
-  providers: [PokemonService]
+  providers: [PokemonService],
 })
 export class PokemonModule {}
