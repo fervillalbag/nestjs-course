69d1801569f24f068478ed6fe290efe018a3cc50
Fernando Villalba <21fervillalbag@gmail.com>
2022-12-26 01:52:41 -0300

    fix: README.md instructions

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,23 +1,41 @@
+import { Model } from 'mongoose';
 import axios, { AxiosInstance } from 'axios';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from '../pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
+  constructor(
+    @InjectModel(Pokemon.name) private readonly pokemonModel: Model<Pokemon>,
+  ) {}
   private readonly axios: AxiosInstance = axios;
 
   async exectuteSeed() {
+    await this.pokemonModel.deleteMany();
+
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=650',
+      'https://pokeapi.co/api/v2/pokemon?limit=5',
     );
+
+    const pokemonToInsert: { name: string; no: number }[] = [];
+
     data.results.forEach((pokemon) => {
       const segments = pokemon.url.split('/');
-      // eslint-disable-next-line
       const no = +segments[segments.length - 2];
 
-      return {};
+      pokemonToInsert.push({
+        name: pokemon.name,
+        no,
+      });
     });
-    return data.results;
+
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
+    return {
+      message: 'Pokemon added on database',
+    };
   }
 }
