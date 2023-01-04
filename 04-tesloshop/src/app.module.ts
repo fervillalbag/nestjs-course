85e83f662d0bf1582b571b282273fa011e36dc2a
Fernando Villalba <21fervillalbag@gmail.com>
2023-01-04 07:36:25 -0300

    init project with mongo

--- 04-tesloshop/src/app.module.ts
+++ 04-tesloshop/src/app.module.ts
@@ -6,14 +6,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.PORT,
-      database: process.env.DB_NAME,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      autoLoadEntities: true,
+      type: 'mongodb',
+      url: process.env.MONGO_URL,
       synchronize: true,
+      logging: true,
     }),
   ],
 })
